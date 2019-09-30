677bf64bc12779509c50ba3292f78deaa11898e5
x-kazuo-x <b1019064@fun.ac.jp>
2019-09-30 15:47:31 +0900

    style: formatted

--- src/games/AnimalCounter.js
+++ src/games/AnimalCounter.js
@@ -1,40 +1,48 @@
 import React from "react";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 //TODO: 必要なライブラリをインポート
 //import hogehoge from "hogehoge"
 
 //AnimalCounterコンポーネントを定義
 class AnimalCounter extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       count: 0,
-      hiki: 'ひき',
+      hiki: "ひき"
     };
 
     //TODO: コンポーネントのstateを初期化
     //this.state = {};
   }
 
-  count_name(hiki){
-    if(hiki%10 === 2 || hiki%10 ===  4 || hiki%10 === 5 
-      || hiki%10 ===  7 || hiki%10 ===  9){
+  count_name(hiki) {
+    if (
+      hiki % 10 === 2 ||
+      hiki % 10 === 4 ||
+      hiki % 10 === 5 ||
+      hiki % 10 === 7 ||
+      hiki % 10 === 9
+    ) {
       this.setState({
-        hiki: 'ひき'
-      })
-    }else if(hiki%10 ===  1 || hiki%10 ===  6 || hiki%10 ===  8 
-      || hiki%10 ===  0){
-        this.setState({
-          hiki: 'ぴき'
-        })}
-        else{
-          this.setState({
-            hiki: 'びき'
-          })
-        }
-    
+        hiki: "ひき"
+      });
+    } else if (
+      hiki % 10 === 1 ||
+      hiki % 10 === 6 ||
+      hiki % 10 === 8 ||
+      hiki % 10 === 0
+    ) {
+      this.setState({
+        hiki: "ぴき"
+      });
+    } else {
+      this.setState({
+        hiki: "びき"
+      });
+    }
   }
 
   render() {
@@ -42,18 +50,21 @@ class AnimalCounter extends React.Component {
 
     return (
       //TODO: DOMを返す
-      
-      <Button variant="contained"
-      onClick={() => {
-        this.setState({
-          count: this.state.count + 1,
-        })
 
-        {this.count_name(this.state.count+1 )}
-      }}
-     >
-       
-      {this.props.animal_name}が{this.state.count}{this.state.hiki}
+      <Button
+        variant="contained"
+        onClick={() => {
+          this.setState({
+            count: this.state.count + 1
+          });
+
+          {
+            this.count_name(this.state.count + 1);
+          }
+        }}
+      >
+        {this.props.animal_name}が{this.state.count}
+        {this.state.hiki}
       </Button>
     );
   }
@@ -61,4 +72,3 @@ class AnimalCounter extends React.Component {
 
 //AnimalCounterをエクスポート
 export default AnimalCounter;
-
